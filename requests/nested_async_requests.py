fb661d32b75d8d2535371d0b2356e98febffe1a2
Victor Deon <victorhad@gmail.com>
2024-12-20 17:33:00 -0300

    Control async with iometer

--- requests/nested_async_requests.py
+++ requests/nested_async_requests.py
@@ -1,4 +1,6 @@
 from httpx import AsyncClient
+from aiometer import run_all
+from functools import partial
 from pokes import pokes
 import asyncio
 
@@ -23,9 +25,17 @@ async def main(pokemon: str) -> None:
             print(f"{pokemon} -> {poke.get('species', {}).get('name')}")
 
 
-async def run_tasks():
+async def run_tasks1():
     tasks = [main(poke) for poke in pokes]
     await asyncio.gather(*tasks)
 
+
+async def run_tasks2():
+    # partial junta o método main com o parâmetro poke
+    tasks = [partial(main, poke) for poke in pokes]
+    # max_at_once: 5 por vez
+    # max_per_second: 10 por segundo
+    await run_all(tasks, max_at_once=5, max_per_second=10)
+
 if __name__ == '__main__':
-    asyncio.run(run_tasks())
+    asyncio.run(run_tasks2())
